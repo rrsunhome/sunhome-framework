bfc1fe9bf231e24541b544264920dd2828543f27
qijia.wang <m15201373116@163.com>
2019-10-19 23:17:47 +0800

    提交章节2-锁机制（不完整）

---
+++ renren-thread/src/main/java/com/renren/thread/core/chapter2/ThreadMethodBootStrap.java
@@ -0,0 +1,102 @@
+package com.renren.thread.core.chapter2;
+
+/**
+ * synchronized修饰在方法或代码块 只要为同一个对象监视器就是同步的，不是同一个对象那么就是异步的
+ */
+public class ThreadMethodBootStrap {
+
+
+    public static void main(String[] args) {
+        PrintService printService = new PrintService();
+
+        ThreadPrintOne threadPrintOne = new ThreadPrintOne(printService);
+        ThreadPrintTwo threadPrintTwo = new ThreadPrintTwo(printService);
+
+        threadPrintOne.start();
+        threadPrintTwo.start();
+
+
+    }
+}
+
+
+class ThreadPrintOne extends Thread {
+
+    private PrintService service;
+
+    public ThreadPrintOne(PrintService threadService) {
+        this.service = threadService;
+    }
+
+    @Override
+    public void run() {
+
+        service.printOne();
+
+
+    }
+}
+
+class ThreadPrintTwo extends Thread {
+
+    private PrintService service;
+
+    public ThreadPrintTwo(PrintService threadService) {
+        this.service = threadService;
+    }
+
+    @Override
+    public void run() {
+
+        service.printTwo();
+
+    }
+}
+
+
+class PrintService {
+
+
+    public synchronized void printOne() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("printOne-" + Thread.currentThread().getName());
+        }
+
+    }
+
+    public void printTwo() {
+        synchronized (this) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("printTwo-" + Thread.currentThread().getName());
+            }
+        }
+
+    }
+
+//    public void printOne() {
+//        for (int i = 0; i < 5; i++) {
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println("printOne-" + Thread.currentThread().getName());
+//        }
+//
+//    }
+//
+//    public void printTwo() {
+//        for (int i = 0; i < 5; i++) {
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println("printTwo-" + Thread.currentThread().getName());
+//        }
+//    }
+
+
+}
+
---
+++ renren-thread/src/main/java/com/renren/thread/core/chapter2/package-info.java
@@ -0,0 +1,9 @@
+package com.renren.thread.core.chapter2;
+
+/**
+ * 多线程基础篇-走进锁机制
+ *
+ * 层面停留在使用层面
+ * 后期补充jvm原理
+ *
+ */
