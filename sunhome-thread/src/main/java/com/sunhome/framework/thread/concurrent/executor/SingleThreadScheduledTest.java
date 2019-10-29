bc05c59e5913a2008e4410f8e5815647f175c5f3
qijia.wang <m15201373116@163.com>
2019-10-29 22:10:16 +0800

    补充线程池代码

--- sunhome-thread/src/main/java/com/sunhome/framework/thread/concurrent/executor/SingleThreadScheduledTest.java
+++ sunhome-thread/src/main/java/com/sunhome/framework/thread/concurrent/executor/SingleThreadScheduledTest.java
@@ -1,22 +1,39 @@
 package com.sunhome.framework.thread.concurrent.executor;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.Executors;
-import java.util.concurrent.RunnableScheduledFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class SingleThreadScheduledTest {
 
     private static ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            System.out.println("任务" + System.currentTimeMillis());
+        Thread timeTask = new Thread(() -> {
+
+            try {
+                System.out.println("begin time:" + LocalDateTime.now().format(dateTimeFormatter));
+
+                // TimeUnit.SECONDS.sleep(3);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
         });
-        executorService.schedule(thread, 2, TimeUnit.SECONDS);
+        // 指定间隔内完成调度，一次性
+        //  executorService.schedule(thread, 2, TimeUnit.SECONDS);
+        // 不延迟，时间每三秒执行一次
+        // 延迟， 任务在上次执行完立马执行
+        //executorService.scheduleAtFixedRate(timeTask, 1, 2, TimeUnit.SECONDS);
+
+        // 每延迟指定时间执行一次(上一次任务的结束与下一次任务的开始为指定时间),延迟和不延迟都是在指定延迟时间内执行
+        executorService.scheduleWithFixedDelay(timeTask, 1, 2, TimeUnit.SECONDS);
+
 
     }
 }
-
-
-
-
