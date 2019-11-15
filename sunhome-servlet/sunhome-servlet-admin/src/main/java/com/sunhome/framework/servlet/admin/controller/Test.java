cc23d5346fa50b0948ef74b911d52afdd21d4e85
lizongren <lizongren@ebupt.com>
2019-11-15 09:38:18 +0800

    添加适配器模式说明

---
+++ sunhome-servlet/sunhome-servlet-admin/src/main/java/com/sunhome/framework/servlet/admin/controller/Test.java
@@ -0,0 +1,14 @@
+package com.sunhome.framework.servlet.admin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Test.class);
+        logger.info("fa");
+        System.out.println("fsd");
+    }
+}
